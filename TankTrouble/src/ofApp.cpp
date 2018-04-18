d703fe559c3d113ea226bf461b7e59d4fa2ae94c
Abhinav Tekulapally <abhitekula@gmail.com>
2018-04-18 16:05:01 -0500

    Fixed tank movement to continously update

--- TankTrouble/src/ofApp.cpp
+++ TankTrouble/src/ofApp.cpp
@@ -2,154 +2,171 @@
 #include <math.h>
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-        // ofDisableAntiAliasing();
-        ofBackgroundHex(0xfdefc2);
-        ofSetLogLevel(OF_LOG_NOTICE);
-        ofSetVerticalSync(true);
+void ofApp::setup() {
+    ofBackgroundHex(0xfdefc2);
+    ofSetLogLevel(OF_LOG_NOTICE);
+    ofSetVerticalSync(true);
 
-        // Box2d
-        box2d.init();
-        box2d.setGravity(0, 0);
-        box2d.createGround();
-        box2d.setFPS(30.0);
+    // Box2d
+    box2d.init();
+    box2d.setGravity(0, 0);
+    box2d.createGround();
+    box2d.setFPS(30.0);
 
-        b2BodyDef tank_body_def;
-        tank_body_def.type = b2_dynamicBody;
-        tank_body_def.position.Set(100,100);
-        tank_body_def.angle = 0;
+    b2BodyDef tank_body_def;
+    tank_body_def.type = b2_dynamicBody;
+    tank_body_def.position.Set(100, 100);
+    tank_body_def.angle = 0;
 
-        tank_body = box2d.getWorld()->CreateBody(&tank_body_def);
+    tank_body = box2d.getWorld()->CreateBody(&tank_body_def);
 
-        //TODO Switch to tank polygon
-        b2PolygonShape rectangle;
-        rectangle.SetAsBox(20,20);
+    //TODO Switch to tank polygon
+    b2PolygonShape rectangle;
+    rectangle.SetAsBox(20, 20);
 
-        b2FixtureDef tank_fixture;
-        tank_fixture.shape = &rectangle;
-        tank_fixture.density = 1;
-        tank_body->CreateFixture(&tank_fixture);
+    b2FixtureDef tank_fixture;
+    tank_fixture.shape = &rectangle;
+    tank_fixture.density = 1;
+    tank_body->CreateFixture(&tank_fixture);
 
-        tank_body->SetAngularVelocity(10);
+    tank_body->SetAngularVelocity(10);
 
-        vector <ofPoint> pts = loadPoints("tank.dat");
-        tank = new ofxBox2dPolygon();
-        tank->addVertices(pts);
-        tank->setPhysics(1.0, 0.3, 0.3);
-        tank->triangulatePoly();
-        tank->body = tank_body;
-        tank->create(box2d.getWorld());
+    vector <ofPoint> pts = loadPoints("tank.txt");
+    tank = new ofxBox2dPolygon();
+    tank->addVertices(pts);
+    tank->setPhysics(1.0, 0.3, 0.3);
+    tank->triangulatePoly();
+    tank->body = tank_body;
+    tank->create(box2d.getWorld());
 }
 
 vector <ofPoint> ofApp::loadPoints(string file) {
-        vector <ofPoint> pts;
-        vector <string>  ptsStr = ofSplitString(ofBufferFromFile(file).getText(), ",");
-        for (int i=0; i<ptsStr.size(); i+=2) {
-                float x = ofToFloat(ptsStr[i]);
-                float y = ofToFloat(ptsStr[i+1]);
-                pts.push_back(ofPoint(x, y));
-        }
-        return pts;
+    vector <ofPoint> pts;
+    vector <string> ptsStr = ofSplitString(ofBufferFromFile(file).getText(),
+                                           ",");
+    for (int i = 0; i < ptsStr.size(); i += 2) {
+        float x = ofToFloat(ptsStr[i]);
+        float y = ofToFloat(ptsStr[i + 1]);
+        pts.push_back(ofPoint(x, y));
+    }
+    return pts;
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-        box2d.update();
+void ofApp::update() {
+    box2d.update();
+    double angle = (tank->body->GetAngle());
+
+    if (isKeyPressed['W']) {
+        tank->body->SetLinearVelocity(b2Vec2(sin(angle) * kLinearVelocity,
+                                             -cos(angle) * kLinearVelocity));
+    }
+
+    if (isKeyPressed['S']) {
+        tank->body->SetLinearVelocity(b2Vec2(-sin(angle) * kLinearVelocity,
+                                             cos(angle) * kLinearVelocity));
+    }
+
+    if (isKeyPressed['A']) {
+        tank->body->SetAngularVelocity(-kAngularVelocity);
+    }
+
+    if (isKeyPressed['D']) {
+        tank->body->SetAngularVelocity(kAngularVelocity);
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-        ofSetHexColor(0x444342);
-        ofFill();
+void ofApp::draw() {
+    ofSetHexColor(0x444342);
+    ofFill();
 
-        tank->draw();
+    tank->draw();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-        int upper_key = toupper(key); // Standardize on upper case
-        double angle = (tank->body->GetAngle());
-        std::cout << "Angle: " << angle << '\n';
+void ofApp::keyPressed(int key) {
+    int upper_key = toupper(key); // Standardize on upper case
+    double angle = (tank->body->GetAngle());
+    std::cout << "Angle: " << angle << '\n';
 
-        if (upper_key == 'W') {
-                tank->body->SetLinearVelocity( b2Vec2(sin(angle), -cos(angle)) );
-        }
-
-        if (upper_key == 'S') {
-                tank->body->SetLinearVelocity( b2Vec2(-sin(angle), cos(angle)) );
-        }
-
-        if (upper_key == 'A') {
-                tank->body->SetAngularVelocity( -2 );
-        }
-
-        if (upper_key == 'D') {
-                tank->body->SetAngularVelocity( 2 );
-        }
+    if (upper_key == 'W') {
+        isKeyPressed[upper_key] = true;
+        tank->body->SetLinearVelocity(b2Vec2(sin(angle) * kLinearVelocity,
+                                             -cos(angle) * kLinearVelocity));
+    } else if (upper_key == 'S') {
+        isKeyPressed[upper_key] = true;
+        tank->body->SetLinearVelocity(b2Vec2(-sin(angle) * kLinearVelocity,
+                                             cos(angle) * kLinearVelocity));
+    } else if (upper_key == 'A') {
+        isKeyPressed[upper_key] = true;
+        tank->body->SetAngularVelocity(-kAngularVelocity);
+    } else if (upper_key == 'D') {
+        isKeyPressed[upper_key] = true;
+        tank->body->SetAngularVelocity(kAngularVelocity);
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-        int upper_key = toupper(key); // Standardize on upper case
+void ofApp::keyReleased(int key) {
+    int upper_key = toupper(key); // Standardize on upper case
 
-        if (upper_key == 'W') {
-                tank->body->SetLinearVelocity( b2Vec2( 0, 0) );
-        }
-
-        if (upper_key == 'S') {
-                tank->body->SetLinearVelocity( b2Vec2( 0, 0) );
-        }
-
-        if (upper_key == 'A') {
-                tank->body->SetAngularVelocity( 0 );
-        }
-
-        if (upper_key == 'D') {
-                tank->body->SetAngularVelocity( 0 );
-        }
+    if (upper_key == 'W') {
+        isKeyPressed[upper_key] = false;
+        tank->body->SetLinearVelocity(b2Vec2(0, 0));
+    } else if (upper_key == 'S') {
+        isKeyPressed[upper_key] = false;
+        tank->body->SetLinearVelocity(b2Vec2(0, 0));
+    } else if (upper_key == 'A') {
+        isKeyPressed[upper_key] = false;
+        tank->body->SetAngularVelocity(0);
+    } else if (upper_key == 'D') {
+        isKeyPressed[upper_key] = false;
+        tank->body->SetAngularVelocity(0);
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- TankTrouble/src/ofApp.h
+++ TankTrouble/src/ofApp.h
@@ -5,27 +5,44 @@
 
 class ofApp : public ofBaseApp {
 
+    //Constants
+    const double kLinearVelocity = 5;
+    const double kAngularVelocity = 2;
+
+private:
+    bool isKeyPressed[255];
+    b2Body *tank_body;
+    ofxBox2d box2d;
+    ofxBox2dPolygon *tank;
+
 public:
-void setup();
-void update();
-void draw();
+    void setup();
 
-void keyPressed(int key);
-void keyReleased(int key);
-void mouseMoved(int x, int y );
-void mouseDragged(int x, int y, int button);
-void mousePressed(int x, int y, int button);
-void mouseReleased(int x, int y, int button);
-void mouseEntered(int x, int y);
-void mouseExited(int x, int y);
-void windowResized(int w, int h);
-void dragEvent(ofDragInfo dragInfo);
-void gotMessage(ofMessage msg);
+    void update();
 
-vector <ofPoint> loadPoints(string file);
+    void draw();
 
-b2Body* tank_body;
-ofPolyline shape;
-ofxBox2d box2d;
-ofxBox2dPolygon* tank;
+    void keyPressed(int key);
+
+    void keyReleased(int key);
+
+    void mouseMoved(int x, int y);
+
+    void mouseDragged(int x, int y, int button);
+
+    void mousePressed(int x, int y, int button);
+
+    void mouseReleased(int x, int y, int button);
+
+    void mouseEntered(int x, int y);
+
+    void mouseExited(int x, int y);
+
+    void windowResized(int w, int h);
+
+    void dragEvent(ofDragInfo dragInfo);
+
+    void gotMessage(ofMessage msg);
+
+    vector <ofPoint> loadPoints(string file);
 };
