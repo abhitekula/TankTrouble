4f38d793750c4fe807a682fff338bd85344ef7d1
Abhinav Tekulapally <abhitekula@gmail.com>
2018-04-18 02:03:21 -0500

    Added box2d dynamic body

--- TankTrouble/src/ofApp.cpp
+++ TankTrouble/src/ofApp.cpp
@@ -1,4 +1,6 @@
 #include "ofApp.h"
+#include "ofxBox2dBaseShape.h"
+#include "ofxBox2dPolygonUtils.h"
 
 //--------------------------------------------------------------
 void ofApp::setup(){
@@ -9,15 +11,35 @@ void ofApp::setup(){
 
         // Box2d
         box2d.init();
-        box2d.setGravity(0, 10);
+        box2d.setGravity(0, 0);
         box2d.createGround();
         box2d.setFPS(30.0);
+
+        b2BodyDef tank_body_def;
+        tank_body_def.type = b2_dynamicBody;
+        tank_body_def.position.Set(100,100);
+        tank_body_def.angle = 0;
+
+        tank_body = box2d.getWorld()->CreateBody(&tank_body_def);
+
+        //TODO Switch to tank polygon
+        b2PolygonShape rectangle;
+        rectangle.SetAsBox(20,20);
+
+        b2FixtureDef tank_fixture;
+        tank_fixture.shape = &rectangle;
+        tank_fixture.density = 1;
+        tank_body->CreateFixture(&tank_fixture);
+
+        tank_body->SetAngularVelocity(10);
 
         vector <ofPoint> pts = loadPoints("tank.dat");
         shared_ptr<ofxBox2dPolygon> poly = shared_ptr<ofxBox2dPolygon>(new ofxBox2dPolygon);
         poly->addVertices(pts);
         poly->setPhysics(1.0, 0.3, 0.3);
         poly->triangulatePoly();
+        // poly.get()->body = tank_body;
+        poly.get()->body = tank_body;
         poly->create(box2d.getWorld());
         polyShapes.push_back(poly);
 }
@@ -35,14 +57,7 @@ vector <ofPoint> ofApp::loadPoints(string file) {
 
 //--------------------------------------------------------------
 void ofApp::update(){
-  polyShapes.clear();
-  vector <ofPoint> pts = loadPoints("tank.dat");
-  shared_ptr<ofxBox2dPolygon> poly = shared_ptr<ofxBox2dPolygon>(new ofxBox2dPolygon);
-  poly->addVertices(pts);
-  poly->setPhysics(1.0, 0.3, 0.3);
-  poly->triangulatePoly();
-  poly->create(box2d.getWorld());
-  polyShapes.push_back(poly);
+        polyShapes[0].get()->body->SetAngularVelocity( 2 );
         box2d.update();
 }
 
@@ -50,7 +65,9 @@ void ofApp::update(){
 void ofApp::draw(){
         ofSetHexColor(0x444342);
         ofFill();
+
         for (int i=0; i<polyShapes.size(); i++) {
+                std::cout << polyShapes[i].get()->body->GetAngularVelocity() << '\n';
                 polyShapes[i]->draw();
         }
 }
--- TankTrouble/src/ofApp.h
+++ TankTrouble/src/ofApp.h
@@ -3,31 +3,29 @@
 #include "ofMain.h"
 #include "ofxBox2d.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+void setup();
+void update();
+void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+void keyPressed(int key);
+void keyReleased(int key);
+void mouseMoved(int x, int y );
+void mouseDragged(int x, int y, int button);
+void mousePressed(int x, int y, int button);
+void mouseReleased(int x, int y, int button);
+void mouseEntered(int x, int y);
+void mouseExited(int x, int y);
+void windowResized(int w, int h);
+void dragEvent(ofDragInfo dragInfo);
+void gotMessage(ofMessage msg);
 
-			vector <ofPoint> loadPoints(string file);
+vector <ofPoint> loadPoints(string file);
 
-		bool                                breakupIntoTriangles;
-ofPolyline                          shape;
-ofxBox2d                            box2d;
-vector <shared_ptr<ofxBox2dCircle> >     circles;
-vector <shared_ptr<ofxBox2dPolygon> >	polyShapes;
-
+b2Body* tank_body;
+ofPolyline shape;
+ofxBox2d box2d;
+vector <shared_ptr<ofxBox2dPolygon> > polyShapes;
 };
