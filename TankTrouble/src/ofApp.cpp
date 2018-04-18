3d813653cd8eaf4402af4e33f627822dc2730815
Abhinav Tekulapally <abhitekula@gmail.com>
2018-04-18 00:32:56 -0500

    Fixed tank to actually look like tank

--- TankTrouble/src/ofApp.cpp
+++ TankTrouble/src/ofApp.cpp
@@ -2,63 +2,57 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofDisableAntiAliasing();
-	ofBackgroundHex(0xfdefc2);
-	ofSetLogLevel(OF_LOG_NOTICE);
-	ofSetVerticalSync(true);
+        ofDisableAntiAliasing();
+        ofBackgroundHex(0xfdefc2);
+        ofSetLogLevel(OF_LOG_NOTICE);
+        ofSetVerticalSync(true);
 
-	// Box2d
-	box2d.init();
-	box2d.setGravity(0, 10);
-	box2d.createGround();
-	box2d.setFPS(30.0);
+        // Box2d
+        box2d.init();
+        box2d.setGravity(0, 10);
+        box2d.createGround();
+        box2d.setFPS(30.0);
 
-
-    breakupIntoTriangles = true;
-
-	// load the shape we saved...
-	vector <ofPoint> pts = loadPoints("tank.dat");
-    shared_ptr<ofxBox2dPolygon> poly = shared_ptr<ofxBox2dPolygon>(new ofxBox2dPolygon);
-    poly->addVertices(pts);
-    poly->setPhysics(1.0, 0.3, 0.3);
-    poly->triangulatePoly();
-    poly->create(box2d.getWorld());
-	polyShapes.push_back(poly);
+        vector <ofPoint> pts = loadPoints("tank.dat");
+        shared_ptr<ofxBox2dPolygon> poly = shared_ptr<ofxBox2dPolygon>(new ofxBox2dPolygon);
+        poly->addVertices(pts);
+        poly->setPhysics(1.0, 0.3, 0.3);
+        poly->triangulatePoly();
+        poly->create(box2d.getWorld());
+        polyShapes.push_back(poly);
 }
 
 vector <ofPoint> ofApp::loadPoints(string file) {
-    vector <ofPoint> pts;
-    vector <string>  ptsStr = ofSplitString(ofBufferFromFile(file).getText(), ",");
-    for (int i=0; i<ptsStr.size(); i+=2) {
-        float x = ofToFloat(ptsStr[i]);
-        float y = ofToFloat(ptsStr[i+1]);
-        pts.push_back(ofPoint(x, y));
-    }
-	return pts;
+        vector <ofPoint> pts;
+        vector <string>  ptsStr = ofSplitString(ofBufferFromFile(file).getText(), ",");
+        for (int i=0; i<ptsStr.size(); i+=2) {
+                float x = ofToFloat(ptsStr[i]);
+                float y = ofToFloat(ptsStr[i+1]);
+                pts.push_back(ofPoint(x, y));
+        }
+        return pts;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-     box2d.update();
+  polyShapes.clear();
+  vector <ofPoint> pts = loadPoints("tank.dat");
+  shared_ptr<ofxBox2dPolygon> poly = shared_ptr<ofxBox2dPolygon>(new ofxBox2dPolygon);
+  poly->addVertices(pts);
+  poly->setPhysics(1.0, 0.3, 0.3);
+  poly->triangulatePoly();
+  poly->create(box2d.getWorld());
+  polyShapes.push_back(poly);
+        box2d.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-  ofSetHexColor(0x444342);
-	ofFill();
-	for (int i=0; i<polyShapes.size(); i++) {
-		polyShapes[i]->draw();
-	}
-  // 
-  // string info = "Draw a shape with the mouse\n";
-	// info += "Press 1 to add some circles\n";
-	// info += "Press c to clear everything\n";
-	// info += "Press t to break object up into triangles/convex poly: "+string(breakupIntoTriangles?"true":"false")+"\n";
-  //   info += "Total Bodies: "+ofToString(box2d.getBodyCount())+"\n";
-	// info += "Total Joints: "+ofToString(box2d.getJointCount())+"\n\n";
-	// info += "FPS: "+ofToString(ofGetFrameRate())+"\n";
-  //   ofSetHexColor(0x444342);
-	// ofDrawBitmapString(info, 10, 15);
+        ofSetHexColor(0x444342);
+        ofFill();
+        for (int i=0; i<polyShapes.size(); i++) {
+                polyShapes[i]->draw();
+        }
 }
 
 //--------------------------------------------------------------
